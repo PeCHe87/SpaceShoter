d6aa9ae3e55d3e9d3e242a9568f7e3ee1b645ebf
Perez, Mauricio <x.Mauricio.Perez@wbgconsultant.com>
2019-04-05 17:30:42 -0300

    Added Health controller and HUD for player's health info.

---
+++ Project Files/Space Shoter/Assets/Scripts/Game/ScriptablePlayer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player", menuName = "GameElements/Player", order = 2)]
+[System.Serializable]
+public class ScriptablePlayer : ScriptableObject
+{
+    public float MaxHealth;
+}
---
+++ Project Files/Space Shoter/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private ScriptablePlayer _playerData = null;
+
+    private HealthController _health = null;
+
+    public HealthController Health
+    {
+        get
+        {
+            return _health;
+        }
+    }
+
+    private void Awake()
+    {
+        _health = GetComponent<HealthController>();
+        _health.SetMaxHealth(_playerData.MaxHealth);
+    }
+}
