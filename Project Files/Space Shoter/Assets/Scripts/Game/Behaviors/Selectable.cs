96e7232df8cc426bf030c3ea72dc82feea7831cc
PeCHe87 <mauriciojperez@hotmail.com>
2019-04-06 14:38:55 -0300

    Added selection logic and selectable component so when an object is selected, if it has selectable component then a UI indicator is shown above it. Added a new weapon scriptable object data for player's weapon.

---
+++ Project Files/Space Shoter/Assets/Scripts/Game/Behaviors/Selectable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+    [SerializeField] private Transform _indicatorPivot = null;
+
+    public Vector3 Pivot
+    {
+        get
+        {
+            return _indicatorPivot.TransformPoint(_indicatorPivot.localPosition);
+        }
+    }
+}
---
+++ Project Files/Space Shoter/Assets/Scripts/Game/Behaviors/SelectionComponent.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class SelectionComponent : MonoBehaviour
+{
+    [SerializeField] private Camera _camera = null;
+    [SerializeField] private Canvas _uiIndicator = null;
+
+    private Selectable _currentSelection = null;
+
+    private void Start()
+    {
+        _uiIndicator.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (_camera == null)
+            return;
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hitInfo);
+
+            if (hit)
+            {
+                // Checks if it is selectable
+                Selectable selectable = hitInfo.transform.GetComponentInParent<Selectable>();
+
+                if (selectable != null)
+                {
+                    // If it was previously selected then hide the selection UI element.
+                    if (selectable.Equals(_currentSelection))
+                        HideIndicator();
+                    else
+                        ShowIndicator(selectable);
+                }
+                else
+                {
+                    // If it isn't a selectable object hides the indicator.
+                    HideIndicator();
+                }
+            }
+            else
+            {
+                // Hides selection UI
+                HideIndicator();
+            }
+        }
+    }
+
+    private void ShowIndicator(Selectable selectable)
+    {
+        // Assigns the current selection
+        _currentSelection = selectable;
+
+        // Show the indicator above this selected object
+        _uiIndicator.transform.position = _currentSelection.Pivot;
+        _uiIndicator.enabled = true;
+    }
+
+    private void HideIndicator()
+    {
+        _uiIndicator.enabled = false;
+        _currentSelection = null;
+    }
+}
